f2b07897a9d011328e0bbd62df43955e63f9d9ef
Ricardo Ferraz Leal <ricardo.leal@ill.eu>
2014-04-09 15:58:56 +0200

    unzip not fully working

--- src/unzip.cpp
+++ src/unzip.cpp
@@ -29,12 +29,26 @@ public:
 
 	}
 
-	void parse(){
+	void parseAll(){
 		//Iterate lines
 		std::string line;
 		while (std::getline(*m_instream, line)) {
 			std::cout << line << std::endl;
 		}
+	}
+
+	std::string getLine(){
+		std::string line;
+		if (std::getline(*m_instream, line))
+			return line;
+		else
+			return NULL;
+	}
+
+	void reset(){
+		m_instream->clear();
+//		m_instream->seekg(0, std::ios::beg);
+//		m_instream->clear();
 	}
 private:
 
@@ -42,26 +56,26 @@ private:
 	boost::shared_ptr<std::ifstream> m_file;
 	boost::shared_ptr< boost::iostreams::filtering_streambuf<boost::iostreams::input> > m_inbuf;
 
-	boost::shared_ptr<std::istream> openFile(const std::string &filename) {
-		//Read from the first command line argument, assume it's gzipped
-		boost::shared_ptr<std::ifstream> file( new std::ifstream(filename, std::ios_base::in | std::ios_base::binary) );
-		m_file = file;
-		boost::shared_ptr< boost::iostreams::filtering_streambuf<boost::iostreams::input> > inbuf(new boost::iostreams::filtering_streambuf<boost::iostreams::input>() );
-		m_inbuf = inbuf;
-		inbuf->push(boost::iostreams::gzip_decompressor());
-		inbuf->push(*file);
-		//Convert streambuf to istream
-		boost::shared_ptr<std::istream> instream(new std::istream(inbuf.get()));
-
-//		std::string line;
-//		while (std::getline(*instream, line)) {
-//			std::cout << line << std::endl;
-//		}
-
-		return instream;
-	}
+	boost::shared_ptr<std::istream> openFile(const std::string &);
 
 };
+
+boost::shared_ptr<std::istream> Parser::openFile(const std::string &filename) {
+	//Read from the first command line argument, assume it's gzipped
+	boost::shared_ptr<std::ifstream> file( new std::ifstream(filename, std::ios_base::in | std::ios_base::binary) );
+	m_file = file;
+	boost::shared_ptr< boost::iostreams::filtering_streambuf<boost::iostreams::input> > inbuf(new boost::iostreams::filtering_streambuf<boost::iostreams::input>() );
+	m_inbuf = inbuf;
+	if (boost::algorithm::ends_with(filename, ".gz")
+			|| boost::algorithm::ends_with(filename, ".GZ")) {
+		inbuf->push(boost::iostreams::gzip_decompressor());
+	}
+	inbuf->push(*file);
+	//Convert streambuf to istream
+	boost::shared_ptr<std::istream> instream(new std::istream(inbuf.get()));
+
+	return instream;
+}
 
 //int main_unzip(int argc, char** argv) {
 //	if (argc < 2) {
@@ -101,7 +115,12 @@ int main_unzip(int argc, char** argv) {
 	}
 	char *filepath = argv[1];
 	Parser p(filepath);
-	p.parse();
+	//p.parseAll();
+	std::cout << p.getLine() << std::endl;
+	std::cout << p.getLine() << std::endl;
+	p.reset();
+	std::cout << p.getLine() << std::endl;
+
 
 	return 0;
 }
