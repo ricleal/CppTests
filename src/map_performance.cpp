1afc6659a3747488ef2adc9d6dc0875e67ed1e77
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-09-17 17:39:22 -0400

    Map erase does not erase... bug?

--- src/map_performance.cpp
+++ src/map_performance.cpp
@@ -4,6 +4,8 @@
 #include <map>
 #include <cstdlib>
 #include <algorithm> //std::find_if
+#include <ctime>
+
 
 class Xpto2000 {
 	std::vector<int> myints;
@@ -33,7 +35,7 @@ void init(std::vector<Xpto2000*> &objects_vector,
 		const unsigned int maxloop) {
 	// initialisations
 	for (unsigned int i = 0; i < maxloop; ++i) {
-		Xpto2000 *m = new Xpto2000(rand());
+		Xpto2000 *m = new Xpto2000(rand() % maxloop + 1);
 		m->fill_vector_random();
 		m->setId(i);
 		objects_vector[i] = m;
@@ -42,6 +44,32 @@ void init(std::vector<Xpto2000*> &objects_vector,
 	}
 }
 
+
+Xpto2000* find_by_id_in_vector_slow(const std::vector<Xpto2000*> &objects_vector, int id){
+	auto it = std::find_if(objects_vector.begin(),
+			objects_vector.end(),
+			[&id](const Xpto2000 *obj) {
+				return obj->getId() == id;
+			});
+	return *it;
+}
+
+Xpto2000* find_by_id_in_vector_fast(const std::vector<Xpto2000*> &objects_vector, int id){
+
+	Xpto2000*  obj = objects_vector[id];
+	std::cout << "\nVec:\t" <<  static_cast<void*>(obj) << " : " << obj->getId() << std::endl;
+	return obj;
+}
+
+void delete_object_in_map_by_memory_address(
+	std::map<void*, Xpto2000*> &objects_map,
+	void* memory_address)  {
+	auto it = objects_map.find(memory_address);
+	if (it != objects_map.end())
+		std::cout << "Map:\t" << it->first << " : " << it->second->getId() << " size = " << objects_map.size() << std::endl;
+		objects_map.erase(it);
+		delete  it->second;
+}
 
 int main_map_performance(int argc, char **argv) {
 	unsigned int maxloop = 100;
@@ -53,24 +81,22 @@ int main_map_performance(int argc, char **argv) {
 
 	init(objects_vector,objects_map,maxloop);
 
+	//seed
+	std::srand(std::time(0));
 	while (objects_map.size() > 0) {
 		int id = std::rand() % maxloop;
 
-		auto it_xpto = std::find_if(objects_vector.begin(),
-				objects_vector.end(),
-				[&id](const Xpto2000 *obj) {
-					return obj->getId() == id;
-				});
-
-		auto it = objects_map.find(static_cast<void*>(*it_xpto));
-		if (it != objects_map.end())
-			objects_map.erase(it);
-
+		auto obj = find_by_id_in_vector_fast(objects_vector,id);
+		delete_object_in_map_by_memory_address(objects_map,
+			static_cast<void*>(obj));
 	}
 
 	// free memory
 	for (auto it = objects_vector.begin(); it != objects_vector.end(); ++it)
-		if (*it) delete *it;
+		if (*it) {
+			//std::cout << "Not Null: " << (*it)->getId() << std::endl;
+			delete *it;
+		}
 
 	return 0;
 }
