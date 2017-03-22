84ac0253604ac02776498ec2a5f05f3e2dda7df9
Ricardo Ferraz Leal <ricleal@gmail.com>
2017-03-22 14:10:40 -0400

    Formatting

--- src/log_binning.cpp
+++ src/log_binning.cpp
@@ -7,7 +7,6 @@
 g++  main.cpp -o test -std=c++11 -stdlib=libc++ && ./test
 */
 
-
 /**
 Same as scipy:
 # Example: Qmin = 0.1 ; Qmax=1; 10 bins
@@ -18,27 +17,26 @@ array([ 0.1       ,  0.12915497,  0.16681005,  0.21544347,  0.27825594,
 Same here returns:
 0.1 0.129155 0.16681 0.215443 0.278256 0.359381 0.464159 0.599484 0.774264 1
 */
-std::vector<double> logBinning(double min, double max, int num){
-	std::vector<double> outBins(num);
-	min = log10(min);
-	max = log10(max);
-	double binWidth = fabs( (max-min)/(num-1) );
-	for(int i = 0; i <= num; ++i){
-		outBins[i] = pow(10, min + i*binWidth);	
-	}
-	return outBins;
+std::vector<double> logBinning(double min, double max, int num)
+{
+    std::vector<double> outBins(num);
+    min = log10(min);
+    max = log10(max);
+    double binWidth = fabs((max - min) / (num - 1));
+    for (int i = 0; i <= num; ++i)
+    {
+	outBins[i] = pow(10, min + i * binWidth);
+    }
+    return outBins;
 }
 
 int main()
 {
-	std::vector<double> v = logBinning(0.1,1,10);
-	std::cout << "--- out:" <<std::endl;
-	for (auto i : v)
-        std::cout << i << ' ';
+    std::vector<double> v = logBinning(0.1, 1, 10);
+    std::cout << "--- out:" << std::endl;
+    for (auto i : v)
+		std::cout << i << ' ';
     std::cout << '\n';
 
-	return 0;
+    return 0;
 }
-
-
-
